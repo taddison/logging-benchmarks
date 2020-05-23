5a4b99ab603dccee1647a0d5fc03f8b0700eb196
Tim Addison <git@tjaddison.com>
2020-05-23 10:45:10 -0400

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace logging_benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
